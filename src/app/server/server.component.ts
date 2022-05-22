ac10455d0efcedbe68afaafff61ede946c8147b7
Minux Dev <minux.midi@gmail.com>
2022-05-21 17:01:08 -0700

    creating and testing styele component and bootstrap framework

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,9 +2,10 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
 })
-
+    
 export class ServerComponent {
-
+    name: string = "Minux developer";
 }
