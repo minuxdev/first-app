791ce2a53091ab94a80ef78da98e8fa9a1ddc795
Minux Dev <minux.midi@gmail.com>
2022-05-21 17:48:53 -0700

    learned how and when to use string interpolation and databinding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,5 +7,9 @@ import { Component } from "@angular/core";
 })
     
 export class ServerComponent {
-    name: string = "Minux developer";
+    keyword: string ;
+
+    classMethod() {
+        return this.keyword;
+    }
 }
