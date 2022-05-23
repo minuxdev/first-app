b1fd1db07356b3aa7c67ad11937a0556392c436b
Minux Dev <minux.midi@gmail.com>
2022-05-23 11:15:24 -0700

    created and used a shared module called ingredients.module.ts in shoppin.component.html

--- courseProject/src/app/components/shopping/shopping.component.ts
+++ courseProject/src/app/components/shopping/shopping.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping',
   templateUrl: './shopping.component.html',
   styleUrls: ['./shopping.component.css']
 })
-export class ShoppingComponent implements OnInit {
 
+export class ShoppingComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Manges', 25),
+    new Ingredient('Orange', 35),
+    new Ingredient('Banana', 226)
+  ]
+  
   constructor() { }
 
   ngOnInit(): void {
---
+++ courseProject/src/app/shared/ingredient.model.ts
@@ -0,0 +1,10 @@
+// another way to create a typescript class
+
+export class Ingredient {
+    constructor(
+        public name: string,
+        public amount: number
+    ) {
+
+    }
+}
